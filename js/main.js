1b61ded4a6f102c2d467510cd49f92d6cd336a42
palaa159 <palaa159@newschool.edu>
2013-07-14 19:46:46 -0400

    7

--- js/main.js
+++
@@ -1,6 +0,0 @@
-$( document ).bind( "mobileinit", function() {
-    // Make your jQuery Mobile framework configuration changes here!
-    $.mobile.buttonMarkup.hoverDelay = 0;
-    $.mobile.allowCrossDomainPages = true;
-    
-});
