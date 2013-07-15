f114d27f2b54aa5af1627f5154ded324614d4249
palaa159 <palaa159@newschool.edu>
2013-07-15 00:09:17 -0400

    10

--- js/main.js
+++ js/main.js
@@ -2,23 +2,26 @@ $(document).delegate("#mapView", "pageinit", function() {
 	var map = L.map('map', {
 		zoomControl: false
 	});
+	var myPos = L.circleMarker([44, 33], 10).addTo(map);
 	L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
 		maxZoom: 18
 	}).addTo(map);
 	// init locate
 
-	function initLocatation() {
+	function findLocation() {
 		map.locate({
+			watch: true,
 			setView: true,
+			minZoom: 10,
 			maxZoom: 16
 		});
 	}
-	initLocatation();
 
 	function onLocationFound(e) {
-		var radius = e.accuracy / 2;
-		L.marker(e.latlng).addTo(map).bindPopup("You are within " + radius + " meters from this point").openPopup();
-		L.circle(e.latlng, radius).addTo(map);
+		var lat = (e.latlng.lat);
+		var lng = (e.latlng.lng);
+		var newLatLng = new L.LatLng(lat, lng);
+		myPos.setLatLng(newLatLng);
 	}
 	map.on('locationfound', onLocationFound);
 
@@ -32,6 +35,9 @@ $(document).delegate("#mapView", "pageinit", function() {
 	});
 	
 	$('#clientLocate').click(function() {
-		initLocatation();
+		findLocation();
 	});
+	
+	
+	findLocation();
 });
