36292479260fd8cff4845146e6dabe6e51bd1ebd
palaa159 <palaa159@newschool.edu>
2013-07-14 22:47:26 -0400

    8

--- js/main.js
+++ js/main.js
@@ -1,33 +1,37 @@
-$( document ).delegate("#mapView", "pageinit", function() {
-  var map = L.map('map', {
-	zoomControl: false
-});
-L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
-	maxZoom: 18
-}).addTo(map);
-// init locate
-
-function initLocatation() {
-	map.locate({
-		setView: true,
-		maxZoom: 16
+$(document).delegate("#mapView", "pageinit", function() {
+	var map = L.map('map', {
+		zoomControl: false
 	});
-}
-initLocatation();
+	L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
+		maxZoom: 18
+	}).addTo(map);
+	// init locate
 
-function onLocationFound(e) {
-	var radius = e.accuracy / 2;
-	L.marker(e.latlng).addTo(map).bindPopup("You are within " + radius + " meters from this point").openPopup();
-	L.circle(e.latlng, radius).addTo(map);
-}
-map.on('locationfound', onLocationFound);
+	function initLocatation() {
+		map.locate({
+			setView: true,
+			maxZoom: 16
+		});
+	}
+	initLocatation();
 
-function onLocationError(e) {
-	alert(e.message);
-}
-map.on('locationerror', onLocationError);
+	function onLocationFound(e) {
+		var radius = e.accuracy / 2;
+		L.marker(e.latlng).addTo(map).bindPopup("You are within " + radius + " meters from this point").openPopup();
+		L.circle(e.latlng, radius).addTo(map);
+	}
+	map.on('locationfound', onLocationFound);
 
-$('#map').css({
-	'height': window.innerHeight - 47
-});
+	function onLocationError(e) {
+		alert(e.message);
+	}
+	map.on('locationerror', onLocationError);
+	
+	$('#map').css({
+		'height': window.innerHeight
+	});
+	
+	$('#clientLocate').click(function() {
+		initLocatation();
+	});
 });
